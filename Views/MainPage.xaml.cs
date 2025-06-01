3b3b7aa8be3d85cb453a41f2e57f17644f3283cb
Anthony Ngan <anthonyngan74@gmail.com>
2025-06-01 15:17:36 +1200

    menu and filter

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -17,5 +17,10 @@ namespace RarezItemWebScraper.Views
 		{
 			ViewModel.SearchText = e.NewTextValue;
 		}
+		private void OnHomeClicked(object sender, EventArgs e) { /* Navigate Home */ }
+		private void OnProductsClicked(object sender, EventArgs e) { /* Navigate or filter to products */ }
+		private void OnSearchClicked(object sender, EventArgs e) { /* Open advanced search */ }
+		private void OnAccountClicked(object sender, EventArgs e) { /* Show account/login page */ }
+		private void OnFilterSearchPressed(object sender, EventArgs e) { /* Optional: trigger filter */ }
 	}
 }
